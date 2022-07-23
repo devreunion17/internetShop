b6ce429dcd771e99bc1eaca5b9486e44b3eda875
devreunion17 <devreunion17@gmail.com>
2022-07-23 17:16:31 +0300

    shop page done

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,46 @@
 $(function () {
 
-$('.shop-content__btn').on('click', function () {
-  $('.shop-content__btn').removeClass('shop-content__btn--active');
-  $(this).addClass('shop-content__btn--active');
-});
-
-$('.button-list').on('click', function () {
-  $('.product').addClass('product--list');
-});
-
-$('.button-grid').on('click', function () {
-  $('.product').removeClass('product--list');
-});
+$('.product-tabs__top-item').on('click', function(e){
+  e.preventDefault();
+  $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+  $(this).addClass('product-tabs__top-item--active');
+ $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+ $($(this).attr('href')).addClass('product-tabs__content-item--active');
+})
 
 
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false
+  });
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true
+  });
 
-  $('.select-style').styler();
+
+  $('.shop-content__btn').on('click', function () {
+    $('.shop-content__btn').removeClass('shop-content__btn--active');
+    $(this).addClass('shop-content__btn--active');
+  });
+
+  $('.button-list').on('click', function () {
+    $('.product').addClass('product--list');
+  });
+
+  $('.button-grid').on('click', function () {
+    $('.product').removeClass('product--list');
+  });
+
+
+
+  $('.select-style, .product-info__item-number').styler();
 
   $('.filter-price__input').ionRangeSlider({
     type: "double",
